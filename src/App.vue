d11ee30daf50c3c4f16b88bac953d27eca730dcd
Charles Chuck <charles.ongao@andela.com>
2018-09-17 14:58:23 +0300

    Conditionally Applying Styles with Style Bindings

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,15 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div calss="content">
     <div class="add-to-cart" @click="addToCart()">Add to Cart</div>
     <div class="top-row">
-      <div class="top part">
+      <div class="top part" :style="headBorderStyle">
         <div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -84,6 +84,13 @@ export default {
     };
   },
   computed: {
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale
+          ? "3px solid red"
+          : "3px solid #aaaaaa"
+      };
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
